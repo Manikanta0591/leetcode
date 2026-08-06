eace78be2f3c607418191633f5473803fd5b4b2a
Manikanta0591 <150006644+Manikanta0591@users.noreply.github.com>
2026-08-06 15:30:20 +0530

    Time: 0 ms (100%), Space: 26.6 MB (44.35%) - LeetHub - Time: O(n), Space: O(n)

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,12 +1,8 @@
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        k = k % n
-        r = [0] * n
-        for i in range(n):
-            r[(i + k) % n] = nums[i]
-        for i in range(n):
-            nums[i] = r[i]
+    def rotate(self, nums: List[int], k: int) -> int:
+        k=k%len(nums)
+        l=len(nums)-k
+        nums[:]=nums[l:]+nums[:l]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
