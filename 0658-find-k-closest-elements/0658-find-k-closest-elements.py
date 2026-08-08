172f1493e9a4b6c634271c974e54bd000bcf7715
Manikanta0591 <150006644+Manikanta0591@users.noreply.github.com>
2026-08-08 15:48:03 +0530

    Time: 0 ms (100%), Space: 20.5 MB (96.39%) - LeetHub - Time: O(log(n-k)), Space: O(1)

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,14 +1,13 @@
 class Solution:
-    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        a=[]
-        for i in range(len(arr)):
-            a.append((abs(x-arr[i]),arr[i]))
-        a.sort()
-        r=[]
-        for i in range(k):
-            r.append(a[i][1])
-        r.sort()
-        return r
+    def findClosestElements(self, arr: list[int], k: int, x: int) -> list[int]:
+        left,right=0,len(arr)-k
+        while left<right:
+            mid=(left+right)//2
+            if x-arr[mid]>arr[mid+k]-x:
+                left=mid+1
+            else:
+                right=mid
+        return arr[left:left+k]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
