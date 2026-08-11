1862b09466ac732f7631f37a8119c3018714ba70
Manikanta0591 <150006644+Manikanta0591@users.noreply.github.com>
2026-08-11 10:17:06 +0530

    Time: 6 ms (9.73%), Space: 20.8 MB (9.85%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,23 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack=[]
+        for ch in tokens:
+            if(ch=="+" or ch=="-" or ch=="*" or ch=="/"):
+                pop1=stack.pop()
+                pop2=stack.pop()
+                if(ch=='+'):
+                    res=int(pop2)+int(pop1)
+                elif(ch=='-'):
+                    res=int(pop2)-int(pop1)
+                elif(ch=='*'):
+                    res=int(pop2)*int(pop1)
+                elif(ch=='/'):
+                    res=int(eval(str(pop2)+"/"+str(pop1)))
+                stack.append(res)
+            else:
+                stack.append(int(ch))
+        return stack[0]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
