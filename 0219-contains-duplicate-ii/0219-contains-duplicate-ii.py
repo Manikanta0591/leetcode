b9e22896b7dbca3d19770c5a74a25cf4c1f74249
Manikanta0591 <150006644+Manikanta0591@users.noreply.github.com>
2026-08-11 11:48:41 +0530

    Time: 43 ms (36.65%), Space: 32.3 MB (79.18%) - LeetHub - Time: O(n), Space: O(k)

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,12 +1,16 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        s=set()
+        s = set()
+
         for i in range(len(nums)):
             if nums[i] in s:
                 return True
+
             s.add(nums[i])
-            if len(s)>k:
-                s.remove(nums[i-k])
+
+            if i >= k:
+                s.remove(nums[i - k])
+
         return False
 
 # Synced seamlessly with LeetHub Pro
