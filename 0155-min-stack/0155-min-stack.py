c21df7bd6cf214ea34c7973d7874c84f8a8d8199
Manikanta0591 <150006644+Manikanta0591@users.noreply.github.com>
2026-08-11 10:08:19 +0530

    Time: 97 ms (56.05%), Space: 31.2 MB (64.21%) - LeetHub - Time: O(1), Space: O(n)

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if not self.minStack:
+            self.minStack.append(val)
+        else:
+            self.minStack.append(min(val, self.minStack[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
