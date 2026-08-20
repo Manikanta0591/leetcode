d225088eecd83366bc4d2c96d0129be48fa0a477
Manikanta0591 <150006644+Manikanta0591@users.noreply.github.com>
2026-08-20 13:41:13 +0530

    Time: 0 ms (100%), Space: 21 MB (93.56%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res=0
+        for n in nums:
+            res=n^res
+        return res
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
