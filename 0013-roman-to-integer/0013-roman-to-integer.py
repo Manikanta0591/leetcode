a6b9a521be76b97f1010730e34af353bd05693db
Manikanta0591 <150006644+Manikanta0591@users.noreply.github.com>
2026-08-20 11:56:03 +0530

    Time: 8 ms (25.75%), Space: 19.5 MB (19.61%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,23 +1,22 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        a={
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+        t=0
+        v={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000,
         }
-        r=0
         for i in range(len(s)):
-            curr=a[s[i]]
-            next_val=a[s[i+1]] if i+1<len(s) else 0
-            if curr<next_val:
-                r-=curr
+            if i+1<len(s) and v[s[i]] < v[s[i+1]]:
+                t-=v[s[i]]
             else:
-                r+=curr
-        return r
+                t+=v[s[i]]
+        return t
+
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
